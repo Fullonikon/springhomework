7b53fc696a1b3f1c5209099b68084bb223f84299
Fullonikon <maxim-199910@mail.ru>
2021-02-17 09:25:38 +0300

    v1

---
+++ src/main/java/com/netcracker/springhomework/configuration/MailConfiguration.java
@@ -0,0 +1,31 @@
+package com.netcracker.springhomework.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfiguration {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("maxim199910@gmail.com");
+        mailSender.setPassword("password");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
--- src/main/java/com/netcracker/springhomework/controller/MainController.java
+++ src/main/java/com/netcracker/springhomework/controller/MainController.java
@@ -5,27 +5,29 @@ import com.netcracker.springhomework.dto.UserWithMeta;
 import com.netcracker.springhomework.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class MainController {
 
-    private FileService fileService;
-
+    private final FileService fileService;
+    final private JavaMailSender emailSender;
 
     @Autowired
-    public MainController(FileService fileService) {
+    public MainController(FileService fileService, JavaMailSender emailSender) {
         this.fileService = fileService;
+        this.emailSender = emailSender;
     }
 
     @PostMapping ("/form")
@@ -48,13 +50,42 @@ public class MainController {
             String[] data = line.split(" ");
             User user = new User(data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
             if (data[0].equals(lastName) && data[1].equals(firstName)){
-                new ResponseEntity<>(new UserWithMeta(user, agent, headers.getDate()), HttpStatus.OK);
+                return new ResponseEntity<>(new UserWithMeta(user, agent, headers.getDate()), HttpStatus.OK);
             }
         }
+        return ResponseEntity.ok().build();
+    }
 
+    @PostMapping("/upload")
+    public ResponseEntity<?> handleFileUpload(@RequestParam("file") MultipartFile file) {
+
+        try {
+            String result = new BufferedReader(new InputStreamReader(file.getInputStream()))
+                    .lines().collect(Collectors.joining("\n"));
+            FileWriter writer = new FileWriter("user.txt", true);
+            writer.write(result);
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return ResponseEntity.ok().build();
     }
 
+    @PostMapping("/send")
+    public ResponseEntity<?> sendMessage(@RequestParam String to, @RequestParam String subject, @RequestParam String text){
+
+        this.sendSimpleMessage(to, subject, text);
+        return ResponseEntity.ok().build();
+    }
+
+    public void sendSimpleMessage(
+            String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
 
 }
